1fb6f762cf20922184918fa2300661f7afe5b0c7
Gabriel Radureau <gradureau@excilys.com>
2018-04-16 11:13:54 +0200

    started model classes

---
+++ src/com/excilys/radureau/computer_database/model/Company.java
@@ -0,0 +1,28 @@
+package com.excilys.radureau.computer_database.model;
+
+public class Company {
+	
+	private Long id;
+	private String name;
+	
+	public Company() {
+	}
+	public Company(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/com/excilys/radureau/computer_database/model/Computer.java
@@ -0,0 +1,53 @@
+package com.excilys.radureau.computer_database.model;
+
+import java.util.Date;
+
+public class Computer {
+	
+	private Long id;
+	private String name;
+	private Date introduced;
+	private Date discontinued;
+	private Company company;
+	
+	public Computer() {
+	}
+	public Computer(Long id, String name, Date introduced, Date discontinued, Company company) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.introduced = introduced;
+		this.discontinued = discontinued;
+		this.company = company;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(Date introduced) {
+		this.introduced = introduced;
+	}
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(Date discontinued) {
+		this.discontinued = discontinued;
+	}
+	public Company getCompany() {
+		return company;
+	}
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+}
