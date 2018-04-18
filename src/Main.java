64f8996a901821117dd2c6bb156133a19110d7f2
Gabriel Radureau <gradureau@excilys.com>
2018-04-18 16:44:21 +0200

    refactoring : moved interactive loop logic to ui.CLI

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.time.LocalDateTime;
-import java.util.Scanner;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.excilys.gradureau.computer_database.model.Computer;
 import com.excilys.gradureau.computer_database.persistance.ConnectionMysqlSingleton;
 import com.excilys.gradureau.computer_database.service.ICrudCDB;
 import com.excilys.gradureau.computer_database.service.ServiceCrudCDB;
@@ -19,96 +15,12 @@ public class Main {
 		logger.warn("Application in Beta, no Validation implemented yet !");
 
 		System.out.println("Hello World !");
-		Scanner scan = new Scanner(System.in);
-
+		
 		ICrudCDB cdb = new ServiceCrudCDB(ConnectionMysqlSingleton.getInstance());
-
-		MAIN_LOOP: while(true) {
-			CLI.listerActions();
-			switch(scan.nextInt()) {
-			default:
-			case CLI.ACTION_QUIT: break MAIN_LOOP;
-			case CLI.ACTION_LIST_COMPUTERS:
-				logger.info("list computers");
-				{
-					System.out.println("Enter a page number");
-					int pageNumber = scan.nextInt();
-					int pageSize = 20;
-					CLI.readPage(pageNumber, pageSize, cdb::listComputers);
-					scan.nextLine(); // move the Scanner cursor to the right position
-				}
-				break;
-			case CLI.ACTION_LIST_COMPANIES:
-				logger.info("list companies");
-				{
-					System.out.println("Enter a page number");
-					int pageNumber = scan.nextInt();
-					int pageSize = 20;
-					CLI.readPage(pageNumber, pageSize, cdb::listCompanies);
-					scan.nextLine(); // move the Scanner cursor to the right position
-				}
-				break;
-			case CLI.ACTION_SHOW_COMPUTER_DETAILS:
-				logger.info("show computer details");
-				{
-					Computer computer = new Computer();
-					System.out.println("Enter a known computer id.");
-					computer.setId(scan.nextLong());
-					System.out.println(
-							cdb.showComputerDetails(computer)
-							);
-					scan.nextLine(); // move the Scanner cursor to the right position
-				}
-				break;
-			case CLI.ACTION_CREATE_COMPUTER:
-				{
-					System.out.println("Enter a name");
-					String name = scan.next();
-					logger.debug("name = " + name);
-					scan.nextLine(); // move the Scanner cursor to the right position
-					LocalDateTime introducedDateTime = CLI.readDate(scan, "introduced");
-					LocalDateTime discontinuedDateTime = CLI.readDate(scan, "discontinued");
-					System.out.println("Enter an eventual company id");
-					String longInput = scan.nextLine();
-					Long companyId = null;
-					try {
-						companyId = Long.valueOf(longInput);
-					}catch(NumberFormatException e) {
-						logger.debug("We could not get the company id.");
-					}
-					logger.debug("company id = " + companyId);
-					Computer newComputer = cdb.createComputer( new Computer(
-							null,
-							name,
-							introducedDateTime,
-							discontinuedDateTime,
-							null
-							),
-							companyId);
-					logger.info("new Computer created :\n\t" + newComputer);
-				}
-				break;
-			case CLI.ACTION_UPDATE_COMPUTER:
-				logger.info("not implemented");
-				//cdb.updateComputer(null);
-				break;
-			case CLI.ACTION_DELETE_COMPUTER:
-				logger.info("delete computer");
-				{
-					Computer computer = new Computer();
-					System.out.println("Enter a known computer id.");
-					computer.setId(scan.nextLong());
-					cdb.deleteComputer(computer);
-					scan.nextLine(); // move the Scanner cursor to the right position
-				}
-				break;
-			}
-			System.out.println("Press [ENTER] to continue.");
-			scan.nextLine();
-		}
-
+		
+		CLI.interactive(cdb);
+		
 		System.out.println("Thank you for using our service !");
-		scan.close();
 	}
 
 }
--- src/com/excilys/gradureau/computer_database/ui/CLI.java
+++ src/com/excilys/gradureau/computer_database/ui/CLI.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.excilys.gradureau.computer_database.model.Computer;
+import com.excilys.gradureau.computer_database.service.ICrudCDB;
 import com.excilys.gradureau.computer_database.util.Page;
 import com.excilys.gradureau.computer_database.util.Pageable;
 
@@ -65,6 +67,96 @@ public class CLI {
 				( date != null ? date.toString() : "null")
 				);	
 		return date == null ? null : date.atStartOfDay();
+	}
+
+	public static void interactive(ICrudCDB cdb) {
+		Scanner scan = new Scanner(System.in);
+	
+		MAIN_LOOP: while(true) {
+			listerActions();
+			switch(scan.nextInt()) {
+			default:
+			case ACTION_QUIT: break MAIN_LOOP;
+			case ACTION_LIST_COMPUTERS:
+				logger.info("list computers");
+				{
+					System.out.println("Enter a page number");
+					int pageNumber = scan.nextInt();
+					int pageSize = 20;
+					readPage(pageNumber, pageSize, cdb::listComputers);
+					scan.nextLine(); // move the Scanner cursor to the right position
+				}
+				break;
+			case ACTION_LIST_COMPANIES:
+				logger.info("list companies");
+				{
+					System.out.println("Enter a page number");
+					int pageNumber = scan.nextInt();
+					int pageSize = 20;
+					readPage(pageNumber, pageSize, cdb::listCompanies);
+					scan.nextLine(); // move the Scanner cursor to the right position
+				}
+				break;
+			case ACTION_SHOW_COMPUTER_DETAILS:
+				logger.info("show computer details");
+				{
+					Computer computer = new Computer();
+					System.out.println("Enter a known computer id.");
+					computer.setId(scan.nextLong());
+					System.out.println(
+							cdb.showComputerDetails(computer)
+							);
+					scan.nextLine(); // move the Scanner cursor to the right position
+				}
+				break;
+			case ACTION_CREATE_COMPUTER:
+				{
+					System.out.println("Enter a name");
+					String name = scan.next();
+					logger.debug("name = " + name);
+					scan.nextLine(); // move the Scanner cursor to the right position
+					LocalDateTime introducedDateTime = readDate(scan, "introduced");
+					LocalDateTime discontinuedDateTime = readDate(scan, "discontinued");
+					System.out.println("Enter an eventual company id");
+					String longInput = scan.nextLine();
+					Long companyId = null;
+					try {
+						companyId = Long.valueOf(longInput);
+					}catch(NumberFormatException e) {
+						logger.debug("We could not get the company id.");
+					}
+					logger.debug("company id = " + companyId);
+					Computer newComputer = cdb.createComputer( new Computer(
+							null,
+							name,
+							introducedDateTime,
+							discontinuedDateTime,
+							null
+							),
+							companyId);
+					logger.info("new Computer created :\n\t" + newComputer);
+				}
+				break;
+			case ACTION_UPDATE_COMPUTER:
+				logger.info("not implemented");
+				//cdb.updateComputer(null);
+				break;
+			case ACTION_DELETE_COMPUTER:
+				logger.info("delete computer");
+				{
+					Computer computer = new Computer();
+					System.out.println("Enter a known computer id.");
+					computer.setId(scan.nextLong());
+					cdb.deleteComputer(computer);
+					scan.nextLine(); // move the Scanner cursor to the right position
+				}
+				break;
+			}
+			System.out.println("Press [ENTER] to continue.");
+			scan.nextLine();
+		}
+		
+		scan.close();
 	}
 
 }
