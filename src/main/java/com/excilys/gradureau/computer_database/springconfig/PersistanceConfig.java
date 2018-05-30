b9edbde5997f7fce499f33de8e787261b1a2f2b2
Gabriel Radureau <gradureau@excilys.com>
2018-05-30 11:18:12 +0200

    [SPRING-JPA] configured hibernate with jpa (hopefully)

--- src/main/java/com/excilys/gradureau/computer_database/springconfig/PersistanceConfig.java
+++ src/main/java/com/excilys/gradureau/computer_database/springconfig/PersistanceConfig.java
@@ -1,20 +1,25 @@
 package com.excilys.gradureau.computer_database.springconfig;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan("com.excilys.gradureau.computer_database.persistance.dao")
 @Import(DataSourceConfig.class)
-public class PersistanceConfig implements TransactionManagementConfigurer {
+public class PersistanceConfig {
     DataSourceConfig dataSourceConfig = new DataSourceConfig();
     
     @Bean
@@ -25,14 +30,37 @@ public class PersistanceConfig implements TransactionManagementConfigurer {
     }
     
     @Bean
-    public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(
-                dataSourceConfig.hikariDataSource()
-                );
+    public PlatformTransactionManager transactionManager(){
+       JpaTransactionManager transactionManager
+         = new JpaTransactionManager();
+       transactionManager.setEntityManagerFactory(
+         entityManagerFactory().getObject() );
+       return transactionManager;
     }
-
-    @Override
-    public PlatformTransactionManager annotationDrivenTransactionManager() {
-        return txManager();
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+       LocalContainerEntityManagerFactoryBean em 
+         = new LocalContainerEntityManagerFactoryBean();
+       em.setDataSource(dataSourceConfig.hikariDataSource());
+       em.setPackagesToScan(new String[] { "com.excilys.gradureau.computer_database.model" });
+  
+       JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+       em.setJpaVendorAdapter(vendorAdapter);
+       em.setJpaProperties(additionalProperties());
+  
+       return em;
+    }
+    
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(
+          "hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        return properties;
+    }
+    
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
     }
 }
