56df8b21fdb97968ba5f07373de3314bbcb41500
Gabriel Radureau <gradureau@excilys.com>
2018-05-31 11:33:22 +0200

    [JPA-CRITERIA] Corrected criteria queries, almost implemented all CompanyDao queries with Criteria

--- src/main/java/com/excilys/gradureau/computer_database/persistance/dao/CompanyDAO.java
+++ src/main/java/com/excilys/gradureau/computer_database/persistance/dao/CompanyDAO.java
@@ -1,6 +1,5 @@
 package com.excilys.gradureau.computer_database.persistance.dao;
 
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -15,7 +14,6 @@ import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,17 +34,8 @@ public class CompanyDAO extends DAO<Company> {
     @Autowired
     EntityManager entityManager;
 
-    private static final String QUERY_LIMIT_ALL = "SELECT id, name FROM company order by name LIMIT ?, ? ;";
     private static final String QUERY_DELETE = "DELETE FROM company WHERE id = ?;";
     private static final String QUERY_DELETE_CHILDREN = "DELETE FROM computer WHERE company_id = ?;";
-    private static final String QUERY_COUNT = "SELECT Count(id) as total FROM company";
-    
-    private static RowMapper<Company> companyRowMapper = (ResultSet rs, int rowNum) -> {
-        Company company = new Company();
-        company.setId(rs.getLong("id"));
-        company.setName(rs.getString("name"));
-        return company;
-    };
 
     @Override
     public Optional<Company> find(long id) {
@@ -88,22 +77,26 @@ public class CompanyDAO extends DAO<Company> {
 
     @Override
     public List<Company> findAll() {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Company> criteriaQuery = criteriaBuilder.createQuery(Company.class);
-        Root<Company> from = criteriaQuery.from(Company.class);
-        criteriaQuery.select(from);
-        TypedQuery<Company> query = entityManager.createQuery(criteriaQuery);
-        List<Company> allitems = query.getResultList();
-        return allitems;
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Company> cquery = cb.createQuery(Company.class);
+        Root<Company> companyNode = cquery.from(Company.class);
+        cquery.select(companyNode);
+        cquery.orderBy(cb.asc(companyNode.get(Company_.NAME)));
+        return entityManager.createQuery(cquery).getResultList();
     }
 
     @Override
     public Page<Company> pagination(int start, int resultsCount) {
-        List<Company> companies = jdbcTemplate.query(
-                QUERY_LIMIT_ALL,
-                new Object[] {start, resultsCount},
-                companyRowMapper
-                );
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Company> cquery = cb.createQuery(Company.class);
+        Root<Company> companyNode = cquery.from(Company.class);
+        cquery.select(companyNode);
+        cquery.orderBy(cb.asc(companyNode.get(Company_.NAME)));
+        TypedQuery<Company> query = entityManager
+                .createQuery(cquery)
+                .setFirstResult(start)
+                .setMaxResults(resultsCount);
+        List<Company> companies = query.getResultList();
         return new Page<>(companies, start, resultsCount);
     }
 
@@ -114,7 +107,10 @@ public class CompanyDAO extends DAO<Company> {
 
     @Override
     public long count() {
-        return jdbcTemplate.queryForObject(QUERY_COUNT, Long.class);
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> cquery = cb.createQuery(Long.class);
+        cquery.select(cb.count(cquery.from(Company.class)));
+        return entityManager.createQuery(cquery).getSingleResult();
     }
 
 }
